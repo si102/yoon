c917529de408249c18434db67fb059ed1e241ca7
si102 <siyoon0403@naver.com>
2024-12-04 13:08:06 +0900

    12/2

--- week14/sharkGame/board.c
+++ week14/sharkGame/board.c
@@ -1,27 +1,27 @@
 //
-//  board.c
+//  board.c :각 칸의 상태(파손여부, 동전 등), 상어의 상태 
 //  sharkGame
 //
 
-#include "board.h"
+#include "board.h" //사용자 정의 헤더파일 
 
-// ----- EX. 3 : board ------------
+// ----- EX. 3 : board ------------		:기본 값 
 #define N_COINPOS       12
 #define MAX_COIN        4
 // ----- EX. 3 : board ------------
-// ----- EX. 5 : shark ------------
+// ----- EX. 5 : shark ------------		:한번에 최대이동 수, 현재 위치 
 #define MAX_SHARKSTEP   6
 #define SHARK_INITPOS   -4
 // ----- EX. 5 : shark ------------
-// ----- EX. 3 : board ------------
-static int board_coin[N_BOARD];
-static int board_status[N_BOARD]; //0 - OK, 1 - destroyed
-// ----- EX. 3 : board ------------
+// ----- EX. 3 : board ------------		:1차 배열 보드 선언 20칸 
+static int board_coin[N_BOARD];  
+static int board_status[N_BOARD]; //0 - OK, 1 - destroyed 	:배열 현재 상태 
+// ----- EX. 3 : board ------------		:상어 위치 변수 선언 
 
 
-static int shark_position;
+static int shark_position;		
 
-// ----- EX. 3 : board ------------
+// ----- EX. 3 : board ------------ 	:보드상태 print 
 void board_printBoardStatus(void)
 {
     int i;
@@ -30,8 +30,8 @@ void board_printBoardStatus(void)
     for (i=0;i<N_BOARD;i++)
     {
         printf("|");
-        if (board_status[i] == BOARDSTATUS_NOK)
-            printf("X");
+        if (board_status[i] == BOARDSTATUS_NOK)  
+            printf("X"); //x= 파손보드
         else
             printf("O");
     }
@@ -46,7 +46,7 @@ int board_initBoard(void)
     //variable initialization
     for (i=0;i<N_BOARD;i++)
     {
-        board_status[i] = BOARDSTATUS_OK;
+        board_status[i] = BOARDSTATUS_OK;  
         board_coin[i] = 0;
     }
     
--- week14/sharkGame/board.h
+++ week14/sharkGame/board.h
@@ -11,17 +11,17 @@
 #include <stdlib.h>
 
 // ----- EX. 3 : board ------------
-#define N_BOARD                 20
+#define N_BOARD                 20 //20칸 짜리 게임 
 
 //0 - OK, 1 - destroyed
-#define BOARDSTATUS_NOK          1
-#define BOARDSTATUS_OK           0
+#define BOARDSTATUS_NOK          1 //die
+#define BOARDSTATUS_OK           0 //alive
 
-int board_initBoard(void);
-int board_getBoardStatus(int pos);
-int board_getShartPosition(void);
-int board_stepShark(void);
-int board_getBoardCoin(int pos);
-void board_printBoardStatus(void);
+int board_initBoard(void); //보드 초기화 
+int board_getBoardStatus(int pos);  //특정 칸의 파손 여부 출력 
+int board_getShartPosition(void); //상어위치 출력 
+int board_stepShark(void); //상어 전진 명령 
+int board_getBoardCoin(int pos); //coin 습득 명령 
+void board_printBoardStatus(void); //전체보드의 상태 출력
 // ----- EX. 3 : board ------------
 #endif /* board_h */
